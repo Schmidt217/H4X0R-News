de1ec0424e44e51446992b8e1c497d4f872e1bae
Mike Schmidt <schmidt217@gmail.com>
2022-08-17 10:23:07 -0600

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Michael Schmidt on 8/17/22.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
