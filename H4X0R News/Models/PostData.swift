7314d25cb416dc063a4d8007a11b8a7efb01c58c
Mike Schmidt <schmidt217@gmail.com>
2022-08-17 11:29:44 -0600

    new app written in SwiftUI

---
+++ H4X0R News/Models/PostData.swift
@@ -0,0 +1,22 @@
+//
+//  PostData.swift
+//  H4X0R News
+//
+//  Created by Michael Schmidt on 8/17/22.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let title: String
+    let points: Int
+    let url: String?
+}
---
+++ H4X0R News/Views/WebView.swift
@@ -0,0 +1,30 @@
+//
+//  WebView.swift
+//  H4X0R News
+//
+//  Created by Michael Schmidt on 8/17/22.
+//
+
+import SwiftUI
+import Foundation
+import WebKit
+
+
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
